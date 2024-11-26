e199bb4d8881e0b160dbf86b575f15abafe43500
Deekshithajanardhan <deekshithajanardhan20@gmail.com>
2024-11-26 14:03:51 +0530

    Create Largest of two numbers.c

---
+++ Largest of two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int a,b;
+printf("Enter the two numbers:");
+scanf("%d%d",&a,&b);
+int(a>b)
+  printf("a is greater than b \n");
+if(b>a)
+  printf("b is greater than a \n");
+}
